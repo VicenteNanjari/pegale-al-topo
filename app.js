f25c8931c1b9fb18a9556ae067393021779c13f8
VicenteNanjari <113800738+VicenteNanjari@users.noreply.github.com>
2022-12-03 00:43:38 -0300

    primer commit del topo

---
+++ app.js
@@ -0,0 +1,50 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelectorAll('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft[0].textContent
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+
+
+    hitPosition = randomPosition.id
+}
+
+square.forEach(id =>  {
+    id.addEventListener('mouseup', () => {
+        if(id.id === hitPosition){
+            result = result + 1
+            score.textContent = result
+        }
+    })
+})
+
+
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 1000)
+}
+
+moveMole()
+
+
+function countDown() {
+    currentTime--
+    timeLeft[0].textContent = currentTime
+
+    if(currentTime === 0 ){
+        clearInterval(timerId)
+
+        alert('YA DÉJALO, SI YA SE MURIÓ')
+    }
+
+}
+
+let timerId = setInterval(countDown, 1000)
